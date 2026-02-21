39f8f931262c42055ccb7037a6b8ce94a1b01684
Divyanshi Gupta <divyanshipdn@gmail.com>
2026-02-21 20:21:51 +0530

    Car Java Program

--- src/Demo.java
+++ src/Demo.java
@@ -1,2 +1,15 @@
 public class Demo {
+    static class Demo1 {
+        void multiply(int a, int b) {
+            System.out.println("Result is " + (a * b));
+        }
+        void multiply(int a, int b, int c) {
+            System.out.println("Result is " + (a * b * c));
+        }
+        public static void main(String[] args) {
+            Demo1 obj = new Demo1();
+            obj.multiply(8, 5);
+            obj.multiply(4, 6, 2);
+        }
+    }
 }
